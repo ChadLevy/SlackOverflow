3d44562157c76ac9915de544a6e259e6f8819d98
Chad Levy <ChadLevy@users.noreply.github.com>
2022-08-22 16:52:29 -0500

    Added web project

---
+++ SlackOverflow/SlackOverflow.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SlackOverflow.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
